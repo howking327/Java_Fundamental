8d0387c23845fb099c30ffc363068df8478664dd
howking327 <howking327@gmail.com>
2019-11-27 15:36:14 +0900

    2019-11-27-v1

--- src/java_20191127/ImplementClassDemo.java
+++ src/java_20191127/ImplementClassDemo.java
@@ -1,6 +1,12 @@
 package java_20191127;
+/**
+ 이 클래스는 인터페이스를 테스트합니다.
+ */
 
 public class ImplementClassDemo {
+	/**
+	 이 메서드는 JVM이 호출해주는 메서드 입니다.
+	 */
 	public static void main(String[] args) {
 		InterA a = new ImplementClass();
 		a.mA();

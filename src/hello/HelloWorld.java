1a3a295ed4f039360159a4dbbdb1ef0e778a90e8
howki <howki@DESKTOP-CA94SRV>
2019-12-18 00:29:12 +0900

    2019-12-18-v1

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -4,7 +4,7 @@ import java_20191127.Calculator;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 		Calculator c = new Calculator();
 		System.out.println(c.plus(10,20));
 	}

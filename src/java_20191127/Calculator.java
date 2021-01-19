126cb9c17a1ba9262de46854651f92b3447c1ba3
howking327 <howking327@gmail.com>
2021-01-19 15:07:11 +0900

    2021-01-19-v1

---
+++ src/java_20191127/Calculator.java
@@ -0,0 +1,18 @@
+package java_20191127;
+/**
+ * 
+ * 계산기 클래스 입니다. 더하기만 있는!
+ * @author acorn
+ *
+ */
+public class Calculator {
+	/**
+	 * 인자를 두개를 넣어주면 더해주는 메서드 입니다. 단, 10억 이하를 입력해 주세요!
+	 * @param a  
+	 * @param b 
+	 * @return 
+	 */
+	public int plus(int a, int b){
+		return a + b;
+	}
+}
